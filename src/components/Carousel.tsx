2a3b4b1b0bb8297b1c69121049aeafd0453ff4fd
承澤李 <chentselee@gmail.com>
2020-05-18 14:31:30 +0800

    Removed element selector. Moved mixins and variables to separate files. Stylesheet clean up.

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,63 +1,95 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import "./Carousel.scss";
 
 export interface slide {
-    image?: string;
-    content: JSX.Element;
-};
+  image?: string;
+  content: JSX.Element;
+}
 
 interface props {
-    data: {
-        slides: slide[];
-    }
+  data: {
+    slides: slide[];
+  };
 }
 
 const calcIndex = (index: number, newIndex: number, length: number) => {
-    if (index === 0) {
-        return length - 1;
-    }
-    return newIndex % length;
+  if (index === 0) {
+    return length - 1;
+  }
+  return newIndex % length;
 };
 
 const Carousel = ({ data: { slides } }: props) => {
-    const [isPlaying, setIsPlaying] = useState(true);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [currentTimeout, setCurrentTimeout] = useState<NodeJS.Timeout>();
-    const duration = 5000;
-    useEffect(() => {
-        currentTimeout && clearTimeout(currentTimeout);
-        if (isPlaying) {
-            const timeout = setTimeout(() => setCurrentIndex(calcIndex(currentIndex, currentIndex + 1, slides.length)), duration);
-            setCurrentTimeout(timeout);
-        }
-    }, [isPlaying, currentIndex]);
-    return (
-        <div className="carousel">
-            <div className="carousel-wrapper">
-                {slides.filter((slide, index) => index === currentIndex).map((slide, index) => {
-                    return (
-                        <React.Fragment key={index}>
-                        <div className="carousel-image-wrapper">
-                            <img src={slide.image} alt=""/>
-                        </div>
-                        <div className="carousel-main">
-                            {slide.content}
-                        </div>
-                        </React.Fragment>
-                    );
-                })}
-                <div className="carousel-control">
-                    {slides.map((slide, index) => {
-                        return (
-                            <button key={index} className={`carousel-control-index-button${index === currentIndex ? ' active' : ''}`} onClick={() => setCurrentIndex(index)}></button>
-                        );
-                    })}
-                    <button className={`carousel-control-toggle-button${isPlaying ? ' playing' : ' stopped'}`} onClick={() => setIsPlaying(!isPlaying)}></button>
-                    <button className="carousel-control-previous" onClick={() => {setCurrentIndex(calcIndex(currentIndex, currentIndex - 1, slides.length))}}></button>
-                    <button className="carousel-control-next" onClick={() => {setCurrentIndex(calcIndex(currentIndex, currentIndex + 1, slides.length))}}></button>
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentTimeout, setCurrentTimeout] = useState<NodeJS.Timeout>();
+  const duration = 5000;
+  useEffect(() => {
+    currentTimeout && clearTimeout(currentTimeout);
+    if (isPlaying) {
+      const timeout = setTimeout(
+        () =>
+          setCurrentIndex(
+            calcIndex(currentIndex, currentIndex + 1, slides.length)
+          ),
+        duration
+      );
+      setCurrentTimeout(timeout);
+    }
+  }, [isPlaying, currentIndex]);
+  return (
+    <div className="carousel">
+      <div className="carousel-wrapper">
+        {slides
+          .filter((slide, index) => index === currentIndex)
+          .map((slide, index) => {
+            return (
+              <React.Fragment key={index}>
+                <div className="carousel-image-wrapper">
+                  <img className="carousel-image" src={slide.image} alt="" />
                 </div>
-            </div>
+                <div className="carousel-main">{slide.content}</div>
+              </React.Fragment>
+            );
+          })}
+        <div className="carousel-control">
+          {slides.map((slide, index) => {
+            return (
+              <button
+                key={index}
+                className={`carousel-control-index${
+                  index === currentIndex ? " active" : ""
+                }`}
+                onClick={() => setCurrentIndex(index)}
+              ></button>
+            );
+          })}
+          <button
+            className={`carousel-control-toggle${
+              isPlaying ? " playing" : " stopped"
+            }`}
+            onClick={() => setIsPlaying(!isPlaying)}
+          ></button>
+          <button
+            className="carousel-control-previous"
+            onClick={() => {
+              setCurrentIndex(
+                calcIndex(currentIndex, currentIndex - 1, slides.length)
+              );
+            }}
+          ></button>
+          <button
+            className="carousel-control-next"
+            onClick={() => {
+              setCurrentIndex(
+                calcIndex(currentIndex, currentIndex + 1, slides.length)
+              );
+            }}
+          ></button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;
