03cdb3ab7be66ea706c48d28ec4694b1663bdf86
承澤李 <chentselee@gmail.com>
2020-05-18 15:46:20 +0800

    Removed element selector. Stylesheet clean up.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,33 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./Card.scss";
 
 export interface CardProps {
-    data: {
-        image?: string;
-        title: string;
-        text: string;
-        links?: {
-            name: string;
-            href?: string;
-        }[];
-    }
+  data: {
+    image?: string;
+    title: string;
+    text: string;
+    links?: {
+      name: string;
+      href?: string;
+    }[];
+  };
 }
 
 const Card = (props: CardProps) => {
-    const { data: { image, title, text, links } } = props;
-    return (
-        <article className="card">
-            <header>
-                <img src={image} />
-            </header>
-            <main>
-                <h3>{title}</h3>
-                <p>{text}</p>
-            </main>
-            <footer>
-                {links ? links.map(link => <a href={link.href}><span>{link.name}</span></a>) : ''}
-            </footer>
-        </article>
-    )
-}
+  const {
+    data: { image, title, text, links },
+  } = props;
+  return (
+    <article className="card">
+      <header>
+        <img className="card-image" src={image} />
+      </header>
+      <main className="card-body">
+        <h3>{title}</h3>
+        <p>{text}</p>
+      </main>
+      <footer className="card-footer">
+        {links
+          ? links.map((link) => (
+              <a className="card-link" href={link.href}>
+                <span>{link.name}</span>
+              </a>
+            ))
+          : ""}
+      </footer>
+    </article>
+  );
+};
 
-export default Card
+export default Card;
