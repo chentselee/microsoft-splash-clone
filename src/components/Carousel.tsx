9d219c50cf0870d03f87e3826fcc81d7864563e2
承澤李 <chentselee@gmail.com>
2020-06-23 20:12:31 +0800

    Refactor to use custom hooks

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -10,28 +10,51 @@ export interface CarouselProps {
   slides: SlideProps[];
 }
 
-const calcIndex = (newIndex: number, length: number) => {
-  if (newIndex < 0) return length - 1;
-  return newIndex % length;
-};
-
-const Carousel: React.FC<CarouselProps> = ({ slides }) => {
-  const [isPlaying, setIsPlaying] = useState(true);
+const useAutoPlaySlides = (duration: number, slidesLength: number) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const timeout = useRef<NodeJS.Timeout>();
-  const duration = 5000;
+  const [isPlaying, setIsPlaying] = useState(true);
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  const calcIndex = (newIndex: number, length: number) => {
+    if (newIndex < 0) return length - 1;
+    return newIndex % length;
+  };
+
+  const jumpToIndex = (newIndex: number) => {
+    setCurrentIndex(calcIndex(newIndex, slidesLength));
+  };
+
   useEffect(() => {
-    timeout.current && clearTimeout(timeout.current);
+    timeoutRef.current && clearTimeout(timeoutRef.current);
     if (isPlaying) {
-      timeout.current = setTimeout(
-        () => setCurrentIndex(calcIndex(currentIndex + 1, slides.length)),
+      timeoutRef.current = setTimeout(
+        () => jumpToIndex(currentIndex + 1),
         duration
       );
     }
     return () => {
-      timeout.current && clearTimeout(timeout.current);
+      timeoutRef.current && clearTimeout(timeoutRef.current);
     };
-  }, [isPlaying, currentIndex]);
+  }, [isPlaying, currentIndex, duration, slidesLength]);
+
+  return {
+    currentIndex,
+    setCurrentIndex,
+    isPlaying,
+    setIsPlaying,
+    timeoutRef,
+    jumpToIndex,
+  };
+};
+
+const Carousel: React.FC<CarouselProps> = ({ slides }) => {
+  const {
+    currentIndex,
+    setCurrentIndex,
+    isPlaying,
+    setIsPlaying,
+    jumpToIndex,
+  } = useAutoPlaySlides(5000, slides.length);
   return (
     <div className="carousel">
       <div className="carousel-wrapper">
@@ -69,14 +92,14 @@ const Carousel: React.FC<CarouselProps> = ({ slides }) => {
           <button
             className="carousel-control-previous"
             onClick={() => {
-              setCurrentIndex(calcIndex(currentIndex - 1, slides.length));
+              jumpToIndex(currentIndex - 1);
             }}
             data-testid="carousel-previous"
           ></button>
           <button
             className="carousel-control-next"
             onClick={() => {
-              setCurrentIndex(calcIndex(currentIndex + 1, slides.length));
+              jumpToIndex(currentIndex + 1);
             }}
             data-testid="carousel-next"
           ></button>
